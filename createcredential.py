dc0c138f16324632c2714d2906988adf321cdfdc
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-08-03 09:30:12 +0200

    License everything under EUPL-1.2+
    Fixes #2
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- createcredential.py
+++ createcredential.py
@@ -1,10 +1,23 @@
+# Copyright 2020 Patrick Uiterwijk
+#
+# Licensed under the EUPL-1.2-or-later
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.serialization import load_der_public_key
 from cryptography.hazmat.primitives.hashes import SHA256
 from cryptography.hazmat.primitives.asymmetric.padding import OAEP, MGF1
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
-import base64
 import dbus
 import hashlib
 import os
